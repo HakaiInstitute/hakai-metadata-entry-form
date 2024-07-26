91254ac5c3a474678f32948d00a43638b4ed3f55
Matthew Foster <matthew.foster@hakai.org>
2024-07-26 15:08:02 -0700

    add sentry replay integration

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,15 @@ if (process.env.NODE_ENV === "production")
   Sentry.init({
     dsn:
       "https://8fd4b6885cc447c0b11aa0cb3009b0e3@o56764.ingest.sentry.io/5493983",
-    integrations: [new Integrations.BrowserTracing()],
+    integrations: [new Integrations.BrowserTracing(),
+      Sentry.replayIntegration({
+        // Additional SDK configuration goes in here, for example:
+        maskAllText: false,
+        blockAllMedia: true,
+      }),
+    ],
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 1,
 
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
