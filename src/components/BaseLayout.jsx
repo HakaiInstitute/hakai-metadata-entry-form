ab3b369558be2e3da523225ebe8dce9c668229a1
Matthew Foster <matthew.foster@hakai.org>
2026-02-24 10:42:00 -0800

    remove cioos logo and move org logo to top of side bar (#19)

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -24,13 +24,8 @@ import Platforms from "./Pages/PlatformsSaved";
 import EditPlatform from "./FormComponents/EditSavedPlatform";
 
 const RegionLogo = ({ children }) => {
-  const { language, region } = useParams();
-  const imgPath = `/cioos-${region}-${language}.png`;
   return (
     <Grid container direction="column" spacing={2}>
-      <Grid item xs>
-        <img src={process.env.PUBLIC_URL + imgPath} alt={region} />
-      </Grid>
       <Grid item xs style={{ paddingLeft: "50px" }}>
         {children}
       </Grid>
